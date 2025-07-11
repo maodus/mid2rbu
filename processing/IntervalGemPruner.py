992a8b1aee4ac62325f6e8a674f09694fed38097
maodus <maodus@ualberta.ca>
2025-07-10 18:09:35 -0600

    feat: small pruning improvements

--- processing/IntervalGemPruner.py
+++ processing/IntervalGemPruner.py
@@ -1,31 +1,46 @@
 class IntervalGemPruner():
   def __init__(self):
       pass
+  
+  def _prune_range(self, gems, l, r):
+    gem_dist = r - l
+    gem_offset = gem_dist & 1
+    for i in range(r - gem_offset, l, -2):
+        if i >= 0:
+          gems.pop(i)
 
   def prune_gems(self, gems, min_tick_gap):
-    gem_len = len(gems)
-    l = gem_len - 1
-    r = l - 1
+    # First pass prunes based on gem color
+    # Second pass prunes across frets/lanes/chords
+    prune_pass = 0 
 
-    while r >= 0:
-      l_gem = gems[l]
-      r_gem = gems[r]
+    while prune_pass < 2:
+      gem_len = len(gems)
+      r = gem_len - 1
+      l = r - 1
 
-      gem_dist = l - r
+      while l >= 0:
+        l_gem = gems[l]
+        r_gem = gems[r]
 
-      if not (r_gem.lane ^ l_gem.lane) and l_gem.tick - r_gem.tick <= min_tick_gap * (gem_dist):
-        r -= 1 # Increase window
-        continue
-      elif gem_dist >= 2: # Atleast 3 gems in window
-        # Delete every second gem in interval
-        gem_offset = gem_dist & 1
-        for i in range(l - gem_offset, r - 1, -2):
-          if i >= 0:
-            gems.pop(i)
+        gem_dist = r - l
+        account_lane = bool(l_gem.lane & r_gem.lane) if prune_pass == 0 else True
 
-        l = r
-        r = l - 1
-        continue
+        if account_lane and r_gem.tick - l_gem.tick <= min_tick_gap * gem_dist:
+          l -= 1 # Increase window
+          continue
+        elif gem_dist > 1: # Atleast 2 gems (includes in window
+          self._prune_range(gems, l, r)
 
-      l -= 1
-      r -= 1
+          # Restart from the gem tick that broke the pruning chain
+          r = l
+          l = r - 1
+          continue
+
+        l -= 1
+        r -= 1
+
+      if l < 0 and r > 0:
+        self._prune_range(gems, l, r)
+
+      prune_pass += 1
